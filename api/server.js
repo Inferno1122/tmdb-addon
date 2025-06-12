04af3c6340c4e295c9f099709b0f613f5b61fae2
Inferno1122 <sak30369@gmail.com>
2025-06-12 16:29:21 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,16 +1,2 @@
-const express = require("express");
-const compression = require("compression");
-const path = require("path");
-const addon = require("../addon/index.js");
-
-const server = express();
-
-server.use(compression());
-server.use("/dist", express.static(path.join(__dirname, "../dist")));
-server.use("/favicon.png", express.static(path.join(__dirname, "../public/favicon.png")));
-server.use("/logo.png", express.static(path.join(__dirname, "../public/logo.png")));
-server.use("/background.png", express.static(path.join(__dirname, "../public/background.png")));
-
-server.use(addon);
-
-module.exports = server;
+const addon = require('../addon/index.js');
+module.exports = addon;
