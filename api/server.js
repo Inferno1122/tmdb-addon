b912690c0bdbc325ab7edc78787ea3bc5ff91421
Inferno1122 <sak30369@gmail.com>
2025-06-12 14:19:53 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
-const addon = require('../addon/index.js');
-module.exports = addon;
+const express = require("express");
+const compression = require("compression");
+const addon = require("../addon/index.js");
+const server = express();
+
+server.use(compression());
+server.use("/dist", express.static("dist"));
+server.use("/favicon.png", express.static("public/favicon.png"));
+
+
+server.use(addon);
+
+module.exports = server;
